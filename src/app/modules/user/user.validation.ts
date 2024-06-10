837f157b1e622850a1ea92080c71263496a99f8d
Forhad5194 <Forhadhossain5194@gmail.com>
2024-06-10 12:05:01 +0600

    User setUp is Done.

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const userValidationSchema = z.object({
+    password : z.string({
+        invalid_type_error: 'password must be a string.'
+    }).max(20,{message:"password must be at least 20 characters"
+ }).optional()
+});
+
+
+
+
+
+
+
+
+export  const UserValidation = {
+    userValidationSchema
+}
